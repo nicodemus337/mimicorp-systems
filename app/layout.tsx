bea55cd73e0ea3e93f6f5c4e24f24cc4e4bc3890
Amelia Cronan <amelia.cronan@gmail.com>
2026-04-17 15:32:08 -0500

    dfrd

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,6 +33,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;600;700&family=IBM+Plex+Mono:wght@400;600&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body>
         <div className="site-shell">
           <header className="site-header">
