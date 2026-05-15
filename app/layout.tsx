97e31518198d7bb28b70dda3b520f2edaa55e4f5
Amelia Cronan <amelia.cronan@gmail.com>
2026-05-15 13:36:01 -0500

    asdf

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Mimicorp Books",
-  description: "Emotionally intelligent interactive webbooks from Mimicorp.",
+  title: "mimicorp labs",
+  description: "Mimicorp labs public site.",
   icons: {
     icon: "/assets/images/mimicorp_logo.svg"
   }
