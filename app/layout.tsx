e02dc5c42401c57525edeb4e56697a707a9fd5c5
Amelia Cronan <amelia.cronan@gmail.com>
2026-05-20 10:58:31 -0500

    fruitful brain

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,18 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "mimicorp labs",
   description: "Mimicorp labs public site.",
+  openGraph: {
+    images: [
+      {
+        url: "/assets/images/fruitfulbrain.png",
+        type: "image/png"
+      }
+    ]
+  },
+  twitter: {
+    card: "summary_large_image",
+    images: ["/assets/images/fruitfulbrain.png"]
+  },
   icons: {
     icon: "/assets/images/mimicorp_logo.svg"
   }
